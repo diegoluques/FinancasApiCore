ea3bd105453ef81e210dda7e04af1c6d19a96ecd
Diego Luques <luquesdiego@outlook.com>
2021-01-24 09:53:51 -0400

    Criação da entidade ContaFinanceira

--- src/Financas.Domain/Entities/ContaFinanceira.cs
+++ src/Financas.Domain/Entities/ContaFinanceira.cs
@@ -1,9 +1,34 @@
 ﻿using Financas.Domain.Bases;
+using Financas.Domain.Exceptions;
 
 namespace Financas.Domain.Entities
 {
 	public class ContaFinanceira : Entity
 	{
+		public ContaFinanceira(string nomeContaFinanceira)
+		{
+			NomeContaFinanceira = nomeContaFinanceira;
+		}
 
+		public ContaFinanceira(int idContaFinanceira, string nomeContaFinanceira, int idPessoa)
+		{
+			IdContaFinanceira = idContaFinanceira;
+			NomeContaFinanceira = nomeContaFinanceira;
+			IdPessoa = idPessoa;
+		}
+
+		protected ContaFinanceira() { }
+
+		public int IdContaFinanceira { get; set; }
+		public string NomeContaFinanceira { get; set; }
+		public int IdPessoa { get; set; }
+
+		public void ModificarNomeContaFinanceira(string nomeContaFinanceira)
+		{
+			if (string.IsNullOrEmpty(nomeContaFinanceira))
+				throw new DomainException("O novo nome da conta financeira não pode ser em branco");
+
+			this.NomeContaFinanceira = nomeContaFinanceira;
+		}
 	}
 }
