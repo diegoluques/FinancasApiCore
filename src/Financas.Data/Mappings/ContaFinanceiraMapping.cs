c1cbcdc6e2ca6f608d28919b5f7ccdcc571678e1
Diego Luques <luquesdiego@outlook.com>
2021-01-25 23:23:08 -0400

    Criação do mapping da ContaFinanceira

---
+++ src/Financas.Data/Mappings/ContaFinanceiraMapping.cs
@@ -0,0 +1,17 @@
+﻿using Financas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Financas.Data.Mappings
+{
+	public class ContaFinanceiraMapping
+	{
+		public void Configure(EntityTypeBuilder<ContaFinanceira> builder)
+		{
+			builder.ToTable("contaFinanceira");
+
+			builder.HasKey(c => c.IdContaFinanceira).HasName("idContaFinanceira");
+			builder.Property(c => c.IdContaFinanceira).ValueGeneratedOnAdd();
+		}
+	}
+}
